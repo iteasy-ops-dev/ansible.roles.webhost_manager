312f8d6b799256dc608b23f2db26a51e7e6087ae
iteasy-ops-dev <iteasy.ops.dev@gmail.com>
2024-07-19 09:33:22 +0900

    Delete tests directory

--- tests/roles/ansible.roles.webhost_manager/tests/roles/ansible.roles.webhost_manager/tests/roles/ansible.roles.webhost_manager/tests/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo "Install Ansible Roles."
-ansible-galaxy install --force --ignore-errors -r requirements.yml -p roles
-
-echo "Test Ansible Roles."
-ansible-playbook -i inventory test.yml
