47e0cbc8c06e70d7d55096710483be5363f2950c
Ban Hun <banhun@Banui-MacBookPro.local>
2024-07-19 14:30:28 +0900

    .

--- tests/test.sh
+++ tests/test.sh
@@ -5,3 +5,12 @@ ansible-galaxy install --force --ignore-errors -r requirements.yml -p roles
 
 echo "Test Ansible Roles."
 ansible-playbook -i inventory test.yml -vvvvv
+
+
+
+# userdel iteasy
+# rm -rf /home/iteasy/
+# rm -rf /etc/httpd/logs/vhosts/iteasy/
+# echo > /etc/httpd/conf.d/vhosts.conf
+# echo > /etc/httpd/conf.d/cband.conf
+# /usr/sbin/apachectl -t
